5ff7f17af0e67ed6a273709c05082af91925d13b
Bohdan-Matviienko <worfrese@gmail.com>
2026-01-12 17:32:40 +0200

    Initial commit

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/hw11_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
